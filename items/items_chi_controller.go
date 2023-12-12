7fab737be062f8e8d531e55764df7e04b586fd01
Tommaso Lauria <tommasolauria98@gmail.com>
2023-12-12 22:27:25 +0100

    feat: get all items controller handler

--- items/items_chi_controller.go
+++ items/items_chi_controller.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
 	"github.com/somatom98/zssn/domain"
 )
 
@@ -25,6 +26,16 @@ func (c *ItemsChiController) GetRouter() http.Handler {
 }
 
 func (c *ItemsChiController) getAllItemsHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	items, err := c.itemsRepository.GetAllItems(ctx)
+	if err != nil {
+		rErr := domain.NewError(err)
+		render.Render(w, r, rErr)
+		return
+	}
+
+	render.JSON(w, r, items)
 }
 
 func (c *ItemsChiController) getItemByNameHandler(w http.ResponseWriter, r *http.Request) {
