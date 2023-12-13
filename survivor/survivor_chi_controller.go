05178250674dbf4448a6618f6e7ebb61e23ddc3a
Tommaso Lauria <tommasolauria98@gmail.com>
2023-12-14 00:26:05 +0100

    feat: get survivor

--- survivor/survivor_chi_controller.go
+++ survivor/survivor_chi_controller.go
@@ -21,6 +21,7 @@ func NewChiController(survivorService domain.SurvivorService) *SurvivorChiContro
 func (c *SurvivorChiController) GetRouter() http.Handler {
 	router := chi.NewRouter()
 	router.Get("/", c.getAllSurvivorsHandler)
+	router.Get("/{sid}", c.getSurvivorHandler)
 	return router
 }
 
@@ -35,4 +36,18 @@ func (c *SurvivorChiController) getAllSurvivorsHandler(w http.ResponseWriter, r 
 	}
 
 	render.JSON(w, r, survivors)
+}
+
+func (c *SurvivorChiController) getSurvivorHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	sid := chi.URLParam(r, "sid")
+
+	survivor, err := c.survivorService.GetSurvivor(ctx, sid)
+	if err != nil {
+		rErr := domain.NewError(err)
+		render.Render(w, r, rErr)
+		return
+	}
+
+	render.JSON(w, r, survivor)
 }
