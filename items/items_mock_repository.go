c9fabbbac6259a5cd68243d7932029bae9757376
Tommaso Lauria <tommasolauria98@gmail.com>
2023-12-14 23:40:37 +0100

    fix: items mock repository

--- items/items_mock_repository.go
+++ items/items_mock_repository.go
@@ -8,12 +8,21 @@ import (
 )
 
 type ItemsMockRepository struct {
-	items []domain.Item
+	MockGetAllItems   func() ([]domain.Item, error)
+	MockGetItemByName func(name string) (domain.Item, error)
+}
+
+func (r *ItemsMockRepository) GetAllItems(ctx context.Context) ([]domain.Item, error) {
+	return r.MockGetAllItems()
+}
+
+func (r *ItemsMockRepository) GetItemByName(ctx context.Context, name string) (domain.Item, error) {
+	return r.MockGetItemByName(name)
 }
 
 func NewMockRepository() *ItemsMockRepository {
-	return &ItemsMockRepository{
-		items: []domain.Item{
+	getAllItems := func() ([]domain.Item, error) {
+		return []domain.Item{
 			{
 				Name:   "water",
 				Points: 4,
@@ -30,19 +39,21 @@ func NewMockRepository() *ItemsMockRepository {
 				Name:   "ammunition",
 				Points: 1,
 			},
-		},
+		}, nil
 	}
-}
 
-func (r *ItemsMockRepository) GetAllItems(ctx context.Context) ([]domain.Item, error) {
-	return r.items, nil
-}
-
-func (r *ItemsMockRepository) GetItemByName(ctx context.Context, name string) (domain.Item, error) {
-	for _, item := range r.items {
-		if item.Name == name {
-			return item, nil
+	getItemByName := func(name string) (domain.Item, error) {
+		items, _ := getAllItems()
+		for _, item := range items {
+			if item.Name == name {
+				return item, nil
+			}
 		}
+		return domain.Item{}, errors.New(domain.ErrCodeNotFound)
 	}
-	return domain.Item{}, errors.New(domain.ErrCodeNotFound)
+
+	return &ItemsMockRepository{
+		MockGetAllItems:   getAllItems,
+		MockGetItemByName: getItemByName,
+	}
 }
