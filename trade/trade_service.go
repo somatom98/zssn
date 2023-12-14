24a49793c5cbe57f84dfcf90472a5f87cd46241e
Tommaso Lauria <tommasolauria98@gmail.com>
2023-12-14 20:31:00 +0100

    feat: trade service

---
+++ trade/trade_service.go
@@ -0,0 +1,19 @@
+package trade
+
+import "github.com/somatom98/zssn/domain"
+
+type TradeService struct {
+	itemsRepository     domain.ItemsRepository
+	inventoryRepository domain.InventoryRepository
+}
+
+func NewTradeService(itemsRepository domain.ItemsRepository, inventoryRepository domain.InventoryRepository) *TradeService {
+	return &TradeService{
+		itemsRepository:     itemsRepository,
+		inventoryRepository: inventoryRepository,
+	}
+}
+
+func (s *TradeService) Trade(offerA domain.TradeOffer, offerB domain.TradeOffer) error {
+	return nil
+}
